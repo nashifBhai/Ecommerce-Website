25508a4c8a579ad9d70f0f2187568ba6c4706851
Md. Nashif Iftekhar <nashififtekhar@gmail.com>
2020-12-21 20:08:19 +0600

    added

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\HttP\Livewire\HomeComponent;
+use App\HttP\Livewire\ShopComponent;
+use App\HttP\Livewire\CartComponent;
+use App\HttP\Livewire\CheckoutComponent;
+use App\HttP\Livewire\DetailsComponent;
+use App\HttP\Livewire\User\UserDashboardComponent;
+use App\HttP\Livewire\Admin\AdminDashboardComponent;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', HomeComponent::class);
+Route::get('/shop', ShopComponent::class);
+Route::get('/cart', CartComponent::class);
+Route::get('/checkout', CheckoutComponent::class);
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
+//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//   return view('dashboard');
+//})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
